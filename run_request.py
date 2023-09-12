6c992f1c795b145764aec8e47cd8ef73ff7095d2
Sanjeev Sharma <143121687+ss4692ss@users.noreply.github.com>
2023-09-12 11:50:57 -0400

    request users

--- run_request.py
+++ run_request.py
@@ -1,9 +1,29 @@
 import json
 
 import requests
+
+
+import os
+
+if not os.path.exists("json_files"):
+  os.mkdir("json_files")
+
+
 access_point = "https://api.github.com"
 
-print(access_point)
+# response_text = requests.get(access_point + "/rate_limit").text
 
-response_text = requests.get(access_point + "/rate_limit").text
-print(json.loads(response_text))
+# print(json.loads(response_text))
+
+user_id = "erinata"
+
+response_text = requests.get(access_point + "/users/" + user_id).text
+
+
+
+json_text = json.loads(response_text)
+
+file_name = "json_files/" + user_id + ".json"
+f = open(file_name, "w")
+f.write(json.dumps(json_text))
+f.close()
