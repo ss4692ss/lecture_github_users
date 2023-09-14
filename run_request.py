5dc3912edd1ceb7b75c4e80ce0507f05cdd8abf0
Sanjeev Sharma <143121687+ss4692ss@users.noreply.github.com>
2023-09-14 10:56:31 -0400

    New

--- run_request.py
+++ run_request.py
@@ -1,25 +1,24 @@
 import json
-
 import requests
-
-
 import os
 
 if not os.path.exists("json_files"):
   os.mkdir("json_files")
 
-
 access_point = "https://api.github.com"
 
-# response_text = requests.get(access_point + "/rate_limit").text
+f = open("Token", "r")
+Token = f.read()
+f.close()
 
-# print(json.loads(response_text))
+github_session = requests.Session()
+github_session.auth = ("ss4692ss", Token)
+
+response_text = github_session.get(access_point + "/rate_limit").text
+print(json.loads(response_text))
 
 user_id = "erinata"
-
-response_text = requests.get(access_point + "/users/" + user_id).text
-
-
+response_text = github_session.get(access_point + "/users/" + user_id).text
 
 json_text = json.loads(response_text)
 
